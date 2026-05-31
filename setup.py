274be86faeefa0a64c8c195374374290961e360e
Sujoy Panigrahi <sujoypanigrahi4@gmail.com>
2026-05-31 16:15:34 +0530

    Update README and bump version to 0.2.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="aillmcleaner",
-    version="0.2.0",
+    version="0.2.1",
     packages=find_packages(),
     install_requires=[
         "pandas>=1.5.0",
